6d1b7d866405709b989847a2cadb8dbf7793d134
Perezh02 <humbertoperezjr114@gmail.com>
2023-02-21 20:47:39 +0000

    mixed lists

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,16 @@
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Make a copy of the list using the copy() method
+numbers_copy = numbers.copy()
+
+# Print the original list and the copy
+print("Original list:", numbers)
+print("Copy of list:", numbers_copy)
+
+# Append a new number to the original list
+numbers.append(6)
+
+# Print both lists again to see the difference
+print("Original list after append:", numbers)
+print("Copy of list after append:", numbers_copy)
